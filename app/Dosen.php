e993a0116d3dcde8d804cc41a911a42714f622c2
nuris akbar <nuris.akbar@gmail.com>
2022-03-12 17:33:11 +0700

    Update Dosen.php

--- app/Dosen.php
+++ app/Dosen.php
@@ -9,7 +9,7 @@ class Dosen extends Authenticatable
 {
     protected $table="dosen";
 
-    protected $fillable=['nidn','nama','email','no_hp','kode_fakultas'];
+    protected $fillable=['nidn','nama','email','no_hp','kode_fakultas','kode_dosen'];
 
     protected $primaryKey = "kode_dosen";
 
